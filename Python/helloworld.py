887e8729bed798b147b1d32b69b2ca97193740d1
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-10-15 16:38:35 +0200

    Adds hello world programs

---
+++ Python/helloworld.py
@@ -0,0 +1,8 @@
+""""
+A project outlining the basic input - output menthods in Python
+---------------------------------------------------------------
+"""
+
+# Print a welcome messgae 
+print("hello World!")
+
