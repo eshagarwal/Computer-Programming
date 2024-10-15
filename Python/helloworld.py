2eac4c082c5dc866ab87f45e8f2c58669d8a00c9
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-10-15 16:43:06 +0200

    Implements the basic input - output menthods in Python

--- Python/helloworld.py
+++ Python/helloworld.py
@@ -6,3 +6,8 @@ A project outlining the basic input - output menthods in Python
 # Print a welcome messgae 
 print("hello World!")
 
+# Ask the user for their name
+user_name = input("What is your name?")
+
+# Print the user_name with the welcome message
+print(f"Welcome {user_name} to Python programming!")
