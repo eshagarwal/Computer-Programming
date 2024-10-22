c9960d56e2698b8198b4cd25761672c8b1c76278
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-10-22 15:48:30 +0200

    Implements the basic mathematical operators in py

---
+++ Python/mathematical_operators.py
@@ -0,0 +1,30 @@
+'''
+Today is all about Mathematical Operators
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+'''
+
+# Multiply 10 & 5
+print(f"10 * 5 = {10*5}")
+
+# Minus 6 by 3
+print(f"6 - 3 = {6-3}")
+
+# Times 68.75 by zero
+print(f"68.75 * 0 = {68.75 * 0}")
+
+# Divide 100 by 17 also by zero
+print(f"100/17 = {(100/17): 2f} & 100/0 = 100/0, \"ZeroDivisionError: division by zero\"")
+
+# 100 % 17 
+print(f"100/17 = {100%17}")
+
+# Set 2 to the powrr of 3
+print(f"2^3 = {2**3}")
+
+# Set variable x equal to 5, y to 3, multiply
+x = 5
+y = 3
+print(f"x={x}, y={y}, x*y = {x*y}")
