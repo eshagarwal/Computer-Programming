55c9be04d1e4da0bda6fa494502e2d4a77338ec6
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-10-23 21:37:06 +0200

    Implements operators in cpp

---
+++ C++/Operators/operators.cpp
@@ -0,0 +1,23 @@
+/*
+Today is all about Operators
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+5. Comparational
+6. Logical
+7. Ternary operator
+*/
+#include <iostream>
+
+int main()
+{
+    using std::cin;
+    using std::cout;
+    using std::endl;
+
+    // expression-1
+    cout << "4 + 5 * 6 - 1 = " << 4 + 5 * 6 - 1 << endl;
+
+    return 0;
+}
