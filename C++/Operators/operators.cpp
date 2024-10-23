d20c3550c618f1b8b65afde404490b3874b5f4e3
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-10-23 21:40:32 +0200

    Updates the operators by adding more expressions

--- C++/Operators/operators.cpp
+++ C++/Operators/operators.cpp
@@ -42,5 +42,8 @@ int main()
     int y = x++;
     cout << "x: " << x << " x++: " << y << endl;
 
+    // expression-6
+    cout << "Ternary Operator: (bool ? true_output : false_output) (9 > 7 ? cool : woah) " << (9 > 7 ? "cool" : "woah") << endl;
+
     return 0;
 }
