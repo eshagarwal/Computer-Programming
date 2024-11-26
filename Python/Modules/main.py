0b8e18b012243ac8cf9d730f185614a585bfc33f
Esha Agarwal <esha.agarwal0212@gmail.com>
2024-11-26 15:27:56 +0100

    Refactors the print statements

--- Python/Modules/main.py
+++ Python/Modules/main.py
@@ -2,8 +2,8 @@ from practise_module import random_circle, current_time_and_date
 
 # Call the functions
 circle_area = random_circle()
-print("Radius of the circle:", circle_area[0])
-print("Area of the circle:", circle_area)
+print(f"Radius of the circle:, {circle_area[0]:.2f} cm")
+print(f"Area of the circle:, {circle_area[1]:.2f} cm²")
 
 current_time = current_time_and_date() 
 print("Current time:", current_time[0])
